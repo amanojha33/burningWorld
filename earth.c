411953166fdbe7bbcb0a9cc4a8a1194ff4f89e5e
Aman Ojha <mcs2019010@iiitl.ac.in>
2021-03-14 18:41:41 +0530

    Setup main

---
+++ earth.c
@@ -0,0 +1,137 @@
+/*
+    Author : Aman Ojha
+    Date : 14th MArch, 2021.
+    Creating a VM from scratch.
+*/
+
+#include <stdo.h>
+
+/* 65536 locations -- 2^16 -- Memory we have in our hardware */
+uint16_t memory[UINT16_MAX];
+
+/* Total register 10. 8 are general purpose. */
+enum{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, /* program counter */
+    R_COND,
+    R_COUNT
+};
+uint16_t reg[R_COUNT];
+
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add  */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved (unused) */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+};
+
+/* Condition flags */
+enum
+{
+    FL_POS = 1 << 0, /* P */
+    FL_ZRO = 1 << 1, /* Z */
+    FL_NEG = 1 << 2, /* N */
+};
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2)
+    {
+        printf("Earth1 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for(int j=1;j<argc;j++){
+        if(!read_image(argv[j]))
+        {
+            printf("Failed to load image: %s\n",argv[j]);
+            exit(1);
+        }
+    }
+    {Setup, 12}
+
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                {ADD, 6}
+                break;
+            case OP_AND:
+                {AND, 7}
+                break;
+            case OP_NOT:
+                {NOT, 7}
+                break;
+            case OP_BR:
+                {BR, 7}
+                break;
+            case OP_JMP:
+                {JMP, 7}
+                break;
+            case OP_JSR:
+                {JSR, 7}
+                break;
+            case OP_LD:
+                {LD, 7}
+                break;
+            case OP_LDI:
+                {LDI, 6}
+                break;
+            case OP_LDR:
+                {LDR, 7}
+                break;
+            case OP_LEA:
+                {LEA, 7}
+                break;
+            case OP_ST:
+                {ST, 7}
+                break;
+            case OP_STI:
+                {STI, 7}
+                break;
+            case OP_STR:
+                {STR, 7}
+                break;
+            case OP_TRAP:
+                {TRAP, 8}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                {BAD OPCODE, 7}
+                break;
+        }
+    }
+    {Shutdown, 12}
+}
