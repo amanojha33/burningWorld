7d60f3880216d83479988a905e8fb94a2321424b
Aman Ojha <mcs2019010@iiitl.ac.in>
2021-03-17 20:42:21 +0530

    Sign extension

--- earth.c
+++ earth.c
@@ -135,3 +135,11 @@ int main(int argc, const char* argv[])
     }
     {Shutdown, 12}
 }
+
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
